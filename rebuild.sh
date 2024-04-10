1d7e138a005e5d8e55402ae4928c6571fa2649ba
ArtemisYo <art.stellata@proton.me>
2024-04-10 14:22:45 +0200

    a lot of script fuckery

--- rebuild.sh
+++ rebuild.sh
@@ -4,11 +4,11 @@ git add .
 
 sudo nixos-rebuild --impure --flake ./ switch
 
-git add flake.lock
-
 counter=`cat .counter`
 ((counter=counter+1))
 
 echo "$counter" > .counter
 
+git add .counter
+git add flake.lock
 git commit -m "$counter"
