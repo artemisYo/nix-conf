1c3cec24a214e3305f83587eb5cf9d7c5cd52595
ArtemisYo <art.stellata@proton.me>
2024-04-16 11:57:27 +0200

    24

--- home-manager/niri/eww.nix
+++ home-manager/niri/eww.nix
@@ -1,5 +1,5 @@
 { pkgs, config, lib, ... }: let
-  cfg = config.custom.ironbar;
+  cfg = config.custom.eww;
 in {
   options.custom.eww = with lib; {
     enable = mkEnableOption "custom eww config";
@@ -8,5 +8,20 @@ in {
   config = lib.mkIf cfg.enable {
     programs.eww.enable = true;
     programs.eww.configDir = ./eww;
+    # xdg.dataHome."fonts/symbols/" = {
+    #   source = pkgs.fetchzip {
+    #     url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/NerdFontsSymbolsOnly.zip";
+    #     hash = "";
+    #   };
+    #   recursive = true;
+    # };
+    home.file.".local/share/fonts/symbols" = {
+      source = pkgs.fetchzip {
+        url = "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/NerdFontsSymbolsOnly.zip";
+        hash = "sha256-86iQmX9J3YpWoCVYGvlyi6A/6uJcPa2iU0MgCmekkiE=";
+        stripRoot = false;
+      };
+      recursive = true;
+    };
   };
 }
